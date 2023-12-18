03583df87e7b72f1ea0280336b297e44cb5881b0
antoineOYO <100151942+antoineOYO@users.noreply.github.com>
2023-12-18 16:02:58 +0100

    1812a

--- dump_preprocessing_and_indexing.ps1
+++
@@ -1,7 +0,0 @@
-Start-Transcript -Path "C:\Users\Antoine\Documents\GitHub\opentapioca\logs_ps1\output.log"
-tapioca train-bow D:\latest-all.json.bz2 ; 
-tapioca preprocess D:\latest-all.json.bz2 ; 
-wsl sort -n -k 1 latest-all.unsorted.tsv > wikidata_graph.tsv;
-tapioca compile wikidata_graph.tsv ;
-tapioca compute-pagerank wikidata_graph.npz
-Stop-Transcript
